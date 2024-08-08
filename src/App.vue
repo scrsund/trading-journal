9dd2a6f1ceaff7b6537236782434028b5b4d9f46
scrsund <137787953+scrsund@users.noreply.github.com>
2024-08-08 21:10:42 +0200

    rough draft of trading entry display

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TradingEntries></TradingEntries>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TradingEntries from "./components/TradingEntries.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TradingEntries,
+  },
+};
 </script>
 
 <style>
@@ -19,7 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/TradingEntries.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Trading Journal</h1>
+    <h2>Signal Checklist</h2>
+    <ul>
+      <li>4hr Trend (Uptrend or Downtrend)</li>
+      <li>Limit Order</li>
+      <li>Candle Confirmation</li>
+      <li>Support and Resistance Lines</li>
+      <li>Time of Day</li>
+      <li>Number of Trades Made</li>
+    </ul>
+  </div>
+  <h2>Journal</h2>
+  <div>
+    <label>Entry Time</label>
+    <input />
+    <label>Lot Size</label>
+    <input />
+    <label>Risk (SL loss)</label>
+    <input />
+    <label>Reward (TP profit)</label>
+    <input />
+    <label>Trade Setup</label>
+    <textarea rows="5"></textarea>
+  </div>
+</template>
+
+<style>
+label {
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+}
+</style>
